3eef0d545517ee2bb2574e9dc19b7d22a35cc41a
TheGame Nicorus <nicorus_1@hotmail.com>
2017-03-25 15:40:16 +0700

    update example

--- examples/Example/pages/overrideRenderExample.js
+++ examples/Example/pages/overrideRenderExample.js
@@ -11,7 +11,7 @@ import {
   View,
   Image
 } from 'react-native';
-import Timeline from './react-native-timeline-listview'
+import Timeline from 'react-native-timeline-listview'
 
 export default class Example extends Component {
   constructor(){
@@ -26,34 +26,34 @@ export default class Example extends Component {
         title: 'Archery Training', 
         description: 'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment, since everything you need will be provided for the course. ',
         lineColor:'#009688', 
-        icon: require('./img/archery.png'),
+        icon: require('../img/archery.png'),
         imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240340/c0f96b3a-0fe3-11e7-8964-fe66e4d9be7a.jpg'
       },
       {
         time: '10:45', 
         title: 'Play Badminton', 
         description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net.', 
-        icon: require('./img/badminton.png'),
+        icon: require('../img/badminton.png'),
         imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240405/0ba41234-0fe4-11e7-919b-c3f88ced349c.jpg'
       },
       {
         time: '12:00', 
         title: 'Lunch', 
-        icon: require('./img/lunch.png'),
+        icon: require('../img/lunch.png'),
       },
       {
         time: '14:00', 
         title: 'Watch Soccer', 
         description: 'Team sport played between two teams of eleven players with a spherical ball. ',
         lineColor:'#009688', 
-        icon: require('./img/soccer.png'),
+        icon: require('../img/soccer.png'),
         imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240419/1f553dee-0fe4-11e7-8638-6025682232b1.jpg'
       },
       {
         time: '16:30', 
         title: 'Go to Fitness center', 
         description: 'Look out for the Best Gym & Fitness Centers around me :)', 
-        icon: require('./img/dumbbell.png'),
+        icon: require('../img/dumbbell.png'),
         imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240422/20d84f6c-0fe4-11e7-8f1d-9dbc594d0cfa.jpg'
       }
     ]
